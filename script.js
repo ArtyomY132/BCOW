136f0969e2f0b905e43932e2199297473fb09f14
ArtyomY132 <brathikovartem163@gmail.com>
2024-11-28 19:19:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function changeLanguage(language) {
+    const elements = {
+        "header-title": {
+            "en": "Welcome to Beautiful corners of the world",
+            "ru": "Добро пожаловать в красивые уголки мира"
+        }
+    };
+
+    const headerTitle = document.getElementById("header-title");
+    if (headerTitle) {
+        headerTitle.textContent = elements["header-title"][language];
+    }
+}
